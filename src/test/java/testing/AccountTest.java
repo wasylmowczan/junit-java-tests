48faed3223d4875bfe6a213d10e8a661a40f0847
Wasyl Mowczan <movchanvasyl@gmail.com>
2020-10-20 14:01:21 +0200

    Given-when-then

--- src/test/java/testing/AccountTest.java
+++ src/test/java/testing/AccountTest.java
@@ -3,12 +3,31 @@ package testing;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AccountTest {
 
     @Test
     void myTest() {
+        //given
+        //when
         Account newAccount = new Account();
+
+        //then
         assertFalse(newAccount.isActive(), "Check if account is not active");
     }
+
+    @Test
+    void myTest2() {
+        //given
+        Account newAccount = new Account();
+        assertFalse(newAccount.isActive());
+
+        //when
+        newAccount.activate();
+
+        //then
+        assertTrue(newAccount.isActive());
+    }
+
 }
